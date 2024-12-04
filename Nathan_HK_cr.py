bc1d663b9ec9de2c32eafeaaa9718777284856e8
nathan-hk-is <124222921+nathan-hk-is@users.noreply.github.com>
2024-12-04 13:22:50 -0800

    findNewsPage()

--- Nathan_HK_cr.py
+++ Nathan_HK_cr.py
@@ -2,15 +2,19 @@
 # 2024-11-30
 
 import geopy
+import numpy as np
 import pandas as pd
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import WebDriverException
 import sys
 import time
 
@@ -196,15 +200,15 @@ def getProfileInfo(df):
 def dataByCountry(df):
     """
     Get data from individual countries' websites.
-    
+
     Countries where it works:
     - US: https://www.sec.gov/search-filings
         - works to get IDs
         - forms not tested
-        
+
     Countries in progress:
     - BR: https://dados.gov.br/home
-    
+
     Countries where it doesn't work:
     - AU: https://connectonline.asic.gov.au/RegistrySearch/
         - can't verify ticker
@@ -215,7 +219,7 @@ def dataByCountry(df):
     - SE: https://finanscentralen.fi.se/search/search.aspx
         - can't verify ticker
         - filings are all in different formats, chosen by company
-        
+
     Countries where I can't find the website with filings:
     - CH
     - DE
@@ -228,7 +232,7 @@ def dataByCountry(df):
     gov_id = {'us': []}
     driver = webdriver.Chrome()
     driver.maximize_window()
-    
+
     # US
     driver.get('https://www.sec.gov/search-filings')
     leit = driver.find_element(By.XPATH,
@@ -265,8 +269,94 @@ def dataByCountry(df):
     df.to_csv('symbol_sample.csv')
 
 
+def findNewsPage(df):
+    news_pages = []
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    for i in range(df.shape[0]):
+        if i % 100 == 0:
+            print(i)
+        if type(df.loc[i, 'website']) is not str:  # No web address
+            news_pages.append('')
+            continue
+        if df.loc[i, 'website'][-1] == '/':
+            slash = ''
+        else:
+            slash = '/'
+        for a in ['news', 'press', 'newsroom', 'company-news']:
+            turl = df.loc[i, 'website'] + slash + a
+            # Get static HTML page
+            try:
+                svar = requests.get(turl, timeout=pause * 10)
+            except requests.exceptions.SSLError:
+                print('SSL ERROR', i, df.loc[i, 'symbol'], a)
+                continue
+            except requests.exceptions.Timeout:
+                print('TIMEOUT', i, df.loc[i, 'symbol'], a)
+                continue
+            except requests.exceptions.ConnectionError:
+                print('CONNECTION ERROR', i, df.loc[i, 'symbol'], a)
+                continue
+            except requests.exceptions.MissingSchema:
+                print('MISSING SCHEMA', i, df.loc[i, 'symbol'], a)
+                news_pages.append('')
+                break
+            # Find true URL
+            if str(svar.status_code)[0] in '123':  # Success
+                driver.get(turl)
+                time.sleep(pause)
+                news_pages.append(driver.current_url)
+                break
+            elif str(svar.status_code)[0] == '4':  # Client error
+                continue
+            elif str(svar.status_code)[0] == '5':  # Server error
+                print('SERVER ERROR', i, df.loc[i, 'symbol'])
+                news_pages.append('')
+                break
+        if len(news_pages) > i:
+            continue
+        try:
+            driver.get(df.loc[i, 'website'] + slash + 'ekki_gilt_url')  # 404 page
+            time.sleep(pause)
+        except WebDriverException:
+            print('WDE', i, df.loc[i, 'symbol'])
+            news_pages.append('')
+            continue
+        old_url = driver.current_url
+        # Accept cookies
+        bts = driver.find_elements(By.XPATH, '//button')
+        cookie_text = ['accept all', 'accept all cookies', 'accept cookies',
+                       'i accept', 'allow cookies', 'allow all cookies']
+        for b in bts:
+            if b.text.strip().lower() in cookie_text:
+                try:
+                    b.click()
+                    time.sleep(pause)
+                    break
+                except ElementClickInterceptedException:
+                    continue
+        # Find links
+        alist = driver.find_elements(By.XPATH, '//a')
+        news_text = ['news', 'press', 'newsroom', 'press room',
+                     'company news', 'press releases']
+        for a in alist:
+            if a.text.strip().lower() in news_text:
+                try:
+                    a.click()
+                    time.sleep(pause)
+                except ElementClickInterceptedException:
+                    news_pages.append('')
+                    break
+                if driver.current_url != old_url:
+                    news_pages.append(driver.current_url)
+                    break
+        if len(news_pages) == i:
+            news_pages.append('')
+    df['news_page'] = news_pages
+    df.to_csv('symbol_sample.csv')
+
+
 if __name__ == '__main__':
-    print(df['exchange_short_name'].value_counts())
     if len(sys.argv) < 2:
         print('ERROR! Must include function name.')
     elif sys.argv[1] == 'isOnYahoo':
@@ -275,5 +365,7 @@ if __name__ == '__main__':
         getProfileInfo(df)
     elif sys.argv[1] == 'dataByCountry':
         dataByCountry(df)
+    elif sys.argv[1] == 'findNewsPage':
+        findNewsPage(df)
     else:
         print('ERROR! Invalid function name.')
